b9cf7adb95a828413b9983f29b88f8342ebd4adc
Ramasamy Ramanathan <53431872+RM-RAMASAMY@users.noreply.github.com>
2025-07-12 00:12:11 -0700

    basic login functionality achieved

--- flask-tutorial/flaskr/__init__.py
+++ flask-tutorial/flaskr/__init__.py
@@ -29,4 +29,8 @@ def create_app(test_config=None):
     from . import db
     db.init_app(app)
 
+    # Register the auth blueprint
+    from . import auth
+    app.register_blueprint(auth.bp)
+
     return app
