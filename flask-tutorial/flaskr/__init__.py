3458aff1e97cb97c44d101c593660bdef13d3e83
Ramasamy Ramanathan <53431872+RM-RAMASAMY@users.noreply.github.com>
2025-07-14 09:25:36 -0700

    App Functional

--- flask-tutorial/flaskr/__init__.py
+++ flask-tutorial/flaskr/__init__.py
@@ -18,13 +18,13 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    @app.route('/')
-    def index():
-        return "Welcome to Flask!"
+    # @app.route('/')
+    # def index():
+    #     return "Welcome to Flask!"
     
-    @app.route('/hello')
-    def hello():
-        return "Hello World"
+    # @app.route('/hello')
+    # def hello():
+    #     return "Hello World"
     
     from . import db
     db.init_app(app)
@@ -32,5 +32,9 @@ def create_app(test_config=None):
     # Register the auth blueprint
     from . import auth
     app.register_blueprint(auth.bp)
+
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
 
     return app
